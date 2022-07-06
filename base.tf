fb2213dbe34317a3528ea573359fcdded3059843
willwilson <qtvhao@gmail.com>
2022-07-07 00:22:11 +0700

    `s`

--- base.tf
+++ base.tf
@@ -12,8 +12,25 @@ data "docker_image" "novnc" {
 resource "docker_container" "novnc" {
   image = data.docker_image.novnc.name
   name  = "novnc"
+  env = [
+    "RUN_FLUXBOX=no",
+    "RUN_XTERM=no",
+  ]
   ports {
     internal = 8080
     external = 80
   }
+}
+
+#data "docker_image" "cypress" {
+#  name  = "cypress/included"
+#}
+resource "docker_container" "cypress" {
+  image = "cypress/included:3.4.0" //data.docker_image.cypress.name
+  name  = "cypress"
+  env = [
+    "DISPLAY=${docker_container.novnc.ip_address}:0.0"
+  ]
+  entrypoint = ["cypress"]
+  command = ["open"]
 }
